0b0af716086f9005c3546b5e982edbe3c9965787
razhakhan <65012840+razhakhan@users.noreply.github.com>
2022-01-24 16:50:36 +0530

    Update TopologicalSort.cpp

--- TopologicalSort.cpp
+++ TopologicalSort.cpp
@@ -10,6 +10,7 @@ vector <vector <int>> g;
 stack <int> sk;
 int n,e;
 
+//normal dfs function
 int dfs(int x) {
 	v[x]=true;
 	for(auto i: g[x]) {
@@ -18,41 +19,44 @@ int dfs(int x) {
 		}
 	}
 	sk.push(x);
+	//push the deepest node in the tree to stack
 }
 
 int main() {
-    int t,i;
-    cin>>n>>e;
+	int t,i;
+    	cin>>n>>e;
         
-    //assign memory for new data
-    v.assign(n, false);
-    g.assign(n, vector <int> ());
+    	//assign memory for new data
+    	v.assign(n, false);
+    	g.assign(n, vector <int> ());
         
-    //take edges as input
-    for(i=0; i<e; i++) {
-        int a,b;
-        cin>>a>>b;
-        g[a].pb(b);
-    }
+    	//take edges as input
+    	for(i=0; i<e; i++) {
+        	int a,b;
+        	cin>>a>>b;
+        	g[a].pb(b);
+    	}
  
-	for(i=0; i<n; i++) {
+	//call dfs on all unvisited nodes
+    	for(i=0; i<n; i++) {
 		if(v[i]==false) {
 			dfs(i);
 		}
-	}
+    	}
 	
-    while(sk.empty()==false) {
+	//pop each element from stack and print
+    	while(sk.empty()==false) {
 		int x;
 		x=sk.top();
 		sk.pop();
 		cout<<x<<" ";
 	}
 	
-    //clear previous values
-    v.clear();
-    for(i=0; i<n; i++) {
-        g[i].clear();
-    }
+    	//clear previous values
+    	v.clear();
+    	for(i=0; i<n; i++) {
+        	g[i].clear();
+    	}
 }
 
 /* 
@@ -66,4 +70,9 @@ ip :
 4 1
 2 3
 3 1
+
+op:
+
+5 4 2 3 1 0
+
 */
